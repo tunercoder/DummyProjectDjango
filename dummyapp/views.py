3cb083ee669dc4b417956934c7d640a188fe9a06
tunercoder <mailashishnautiyal.com>
2019-04-12 18:32:49 +0530

    ORM and template parts added

--- dummyapp/views.py
+++ dummyapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blogpost
+from django.utils import timezone
 # Create your views here.
+def post_list(request):
+	blogpost = Blogpost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'dummyapp/post_list.html', {'blogpost':blogpost})
