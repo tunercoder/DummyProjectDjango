3daec20e90b4dcacc3227ae3806382ddb31c4365
tunercoder <mailashishnautiyal.com>
2019-04-15 17:25:06 +0530

    added view and tmeplate for detailed blog post

--- dummyapp/views.py
+++ dummyapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blogpost
 from django.utils import timezone
 # Create your views here.
 def post_list(request):
 	blogpost = Blogpost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'dummyapp/post_list.html', {'blogpost':blogpost})
+
+def post_detail(request, pk):
+    blogpost = get_object_or_404(Blogpost, pk=pk)
+    return render(request, 'dummyapp/post_detail.html', {'blogpost': blogpost})
